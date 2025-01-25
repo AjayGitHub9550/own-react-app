58396a2d7a7c6022e3cd54a026e5eb788e192623
Ajay Y <ajay.yerukula@outlook.com>
2025-01-26 00:10:13 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Welcome to My React App!</h1>
+        <p>This is a simple React app for testing deployment.</p>
+      </div>
+    );
+  }
+  
+  export default App;
+  
